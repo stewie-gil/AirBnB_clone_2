025c703be6a7f0e8cbc16465c128703f641804a2
stewie-gil <gilliganstewie7@gmail.com>
2023-04-23 15:14:14 -0700

    states list fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    sorted_states=sorted(states.values(),key=lambda state: state.name)
+    states = storage.all(State).values()
+    sorted_states=sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
 
 @app.teardown_appcontext
