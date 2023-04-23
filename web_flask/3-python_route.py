0b2db1e40967299d0c87246aebd04cef0e387ce5
stewie-gil <gilliganstewie7@gmail.com>
2023-04-23 10:11:37 -0700

    fixed text

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -20,10 +20,12 @@ def hbnb():
 def c(text):
     return ("C {}".format(text.replace('_', ' ')))
 
+
 @app.route('/python', strict_slashes=False)
-@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
     return("python {}".format(text.replace('_', ' ')))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
