cbd431f7958c07d051d104aeeb4210fe1b953b2b
stewie-gil <gilliganstewie7@gmail.com>
2023-04-23 10:51:49 -0700

    fiexed function

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,7 +33,7 @@ def number(n):
 
 
 @app.route('/number_template/<int=n>', strict_slashes=False)
-def number_template():
+def number_template(n):
     return render_template('5-number.html', number=n)
 
 
