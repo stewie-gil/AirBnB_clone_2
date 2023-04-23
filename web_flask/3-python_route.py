d3c8968589ae01bab5dcf2566ca7200479db4d8b
stewie-gil <gilliganstewie7@gmail.com>
2023-04-23 10:16:23 -0700

    capital p

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,7 @@ def c(text):
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
-    return("python {}".format(text.replace('_', ' ')))
+    return("Python {}".format(text.replace('_', ' ')))
 
 
 if __name__ == "__main__":
